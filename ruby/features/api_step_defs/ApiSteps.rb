026ab7b5fe59660eb3291f6b9c4fb3e88eb08d69
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-10-03 11:32:10 +0100

    Addded in Ruby multi step def example

---
+++ ruby/features/api_step_defs/ApiSteps.rb
@@ -0,0 +1,12 @@
+Given("I am on the login page") do
+    puts "API Steps"
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I submit valid credentials") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am logged in successfully") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
