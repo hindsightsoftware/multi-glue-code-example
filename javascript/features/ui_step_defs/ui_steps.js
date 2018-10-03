73507f8f1196f25842d35caadcf170a57c4a15da
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-10-03 11:54:37 +0100

    Added in JavaScript example for multi-steps

---
+++ javascript/features/ui_step_defs/ui_steps.js
@@ -0,0 +1,15 @@
+const { Given, When, Then } = require('cucumber')
+
+Given('I am on the login page', function () {
+    console.log("UI code!");
+});
+
+When('I submit valid credentials', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I am logged in successfully', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
